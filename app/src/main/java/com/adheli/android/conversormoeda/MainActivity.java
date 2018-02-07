63b419a27025e8c8f422a1080037a4f79a12f6bb
Adheli Tavares <adt@certi.org.br>
2018-02-07 17:08:31 -0400

    finalizando layout

--- app/src/main/java/com/adheli/android/conversormoeda/MainActivity.java
+++ app/src/main/java/com/adheli/android/conversormoeda/MainActivity.java
@@ -2,15 +2,17 @@ package com.adheli.android.conversormoeda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static class ViewHolder {
         EditText editRealValue;
-        EditText editDollarValue;
-        EditText editEuroValue;
+        TextView textDollarValue;
+        TextView textEuroValue;
         Button buttonCalc;
     }
 
@@ -22,8 +24,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.mViewHolder.editRealValue = findViewById(R.id.editRealValue);
-        this.mViewHolder.editDollarValue = findViewById(R.id.editDollarValue);
-        this.mViewHolder.editEuroValue = findViewById(R.id.editEuroValue);
+        this.mViewHolder.textDollarValue = findViewById(R.id.textDollarValue);
+        this.mViewHolder.textEuroValue = findViewById(R.id.textEuroValue);
         this.mViewHolder.buttonCalc = findViewById(R.id.buttonCalc);
+
+        this.mViewHolder.buttonCalc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+
+        if (viewId == R.id.buttonCalc) {
+            this.calculate();
+        }
+    }
+
+    private void calculate() {
+        double realValue = Double.valueOf(this.mViewHolder.editRealValue.getText().toString());
+
+        double dValue = realValue * 3.34;
+        this.mViewHolder.textDollarValue.setText(String.format("%.2f", dValue));
+
+        double eValue = realValue * 4.76;
+        this.mViewHolder.textEuroValue.setText(String.format("%.2f", eValue));
     }
 }
